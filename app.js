54ff639db23d0393a98bada67aa50c18418fde56
Patrik0601 <apatrik0601@gmail.com>
2025-04-10 17:12:27 +0200

    elso

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import { initializeDB, dbAll, dbRun } from './util/database.js';
+import cors from 'cors'
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(bodyParser.json())
+
+app.get('/schedule', async (req, res) => {
+    const rows = await dbAll('SELECT * FROM lessons')
+    res.json(rows)
+})
+
+
+app.post('/schedule', async (req, res) => {
+    const { day, ora1, ora2, ora3, ora4, ora5, ora6, ora7, ora8, ora9 } = req.body
+    await dbRun(
+        `INSERT INTO lessons (day, ora1, ora2, ora3, ora4, ora5, ora6, ora7, ora8, ora9)
+         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [day, ora1, ora2, ora3, ora4, ora5, ora6, ora7, ora8, ora9]
+    )
+    res.sendStatus(200)
+})
+
+app.delete('/schedule/:id', async (req, res) => {
+    await dbRun('DELETE FROM lessons WHERE id = ?', [req.params.id])
+    res.sendStatus(200)
+})
+
+app.put('/schedule/:id', async (req, res) => {
+    const { day, ora1, ora2, ora3, ora4, ora5, ora6, ora7, ora8, ora9 } = req.body
+    await dbRun(
+        `UPDATE lessons SET day=?, ora1=?, ora2=?, ora3=?, ora4=?, ora5=?, ora6=?, ora7=?, ora8=?, ora9=? WHERE id=?`,
+        [day, ora1, ora2, ora3, ora4, ora5, ora6, ora7, ora8, ora9, req.params.id]
+    )
+    res.sendStatus(200)
+})
+
+async function startServer(){
+    await initializeDB();
+    app.listen(3000, () => {
+        console.log("Server runs on port 3000")
+    });
+}
+
+startServer();
